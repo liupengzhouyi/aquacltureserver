08d87dcd1b7800a75eeaa8eaab108c381ecdd654
liupeng <liupeng.0@outlook.com>
2021-01-01 02:03:37 +0800

    Initial commit V 0.0.1

---
+++ src/main/java/cn/liupengstudy/aquacltureserver/AquacltureserverApplication.java
@@ -0,0 +1,13 @@
+package cn.liupengstudy.aquacltureserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AquacltureserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AquacltureserverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/liupengstudy/aquacltureserver/AquacltureserverApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.liupengstudy.aquacltureserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AquacltureserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
